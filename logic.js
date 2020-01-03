94a4557d596bad479d4021840271f0f04cc44e98
Richard T. Holme <Holme_Richard_T@cat.com>
2020-01-03 13:31:46 -0500

    UI update & backend

--- logic.js
+++ logic.js
@@ -1,11 +1,26 @@
 $( document ).ready(function() {
 
+ 	// TODO: Add SDKs for Firebase products that you want to use
+    // https://firebase.google.com/docs/web/setup#available-libraries 
+
+	// Your web app's Firebase configuration
+	var firebaseConfig = {
+		
+	};
+
+	// Initialize Firebase
+	firebase.initializeApp(firebaseConfig);
+
+	var db = firebase.firestore();
+
 	$('.modal').modal();
 
 	var calc_numbers = [];
 
+	var category;
+
 	$('.calc_num').on('click', function(){
-		var num = $(this).text();
+		var num = $(this).text().trim();
 		calc_numbers.push(num);
 		$('.calc_text').text(calc_numbers.join(''));	
 	})
@@ -14,4 +29,48 @@ $( document ).ready(function() {
 		calc_numbers.pop();
 		$('.calc_text').text(calc_numbers.join(''));
 	})
+
+	$('.category_selectors, .money_selectors').on('click', function(){
+		category = $(this).attr('data-value');
+	})
+
+	$('.exit').on('click', function(){
+		clear();
+	})
+
+	$('.add_amount').on('click', function(){
+		var amount = calc_numbers.join('');
+		transaction(category, amount);
+		clear();
+	})
+
+	function clear(){
+		console.log('called')
+		category = '';
+		calc_numbers = [];
+		$('.calc_text').empty();
+	}
+
+	function transaction(cat, amt){
+		console.log(cat);
+		console.log(amt);
+
+		var dateObj = new Date();
+		var month = dateObj.getUTCMonth() + 1; //months from 1-12
+		var day = dateObj.getUTCDate();
+		var year = dateObj.getUTCFullYear();
+
+		newdate = month + "/" + day + "/" + year;
+		var temp_date = dateObj.toString().split(" ");
+		var date = temp_date[1] + " " + temp_date[2] + " " + temp_date[3] + " " + temp_date[4];
+
+		db.collection("transactions").doc(cat).set({[date]: amt}, { merge: true })
+		.then(function() {
+		    console.log("Document successfully written!");
+		}).catch(function(error) {
+		    // The document probably doesn't exist.
+		    console.error("Error updating document: ", error);
+		});
+	}
+
 })
